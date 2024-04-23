e77619252e31a677ab22444fc393b3dce42e4d87
seeker0472 <gmx472@qq.com>
2024-04-23 11:08:02 +0800

    继续完成CVOCR部分

--- quiz_cv.py
+++ quiz_cv.py
@@ -5,11 +5,17 @@ from driver import driver
 import queue
 import sqlite3
 
+from thefuzz import fuzz
+from thefuzz import process
+
 result_queue = queue.Queue()
+# TODO: 未完成
 last_status = None
 
 con = sqlite3.connect('main.sqlite')
 cur = con.cursor()
+# 数据库中的问题
+questions_db = [item[1] for item in cur.execute("SELECT * FROM Questions").fetchall()]
 
 
 def start_answer():
@@ -41,7 +47,7 @@ def answering():
             pass
         elif result['type'] == 'not_selected':
             # 没选
-            answer_question(result)
+            answer_question(result['result'])
             pass
         else:
             # 未知类型
@@ -59,20 +65,72 @@ def update_db(result):
 
 def answer_question(information):
     """
-    回答问题
+    回答问题,只有当没选的时候调用
+    :param information:如果截图中没有选答案,information是{"question": question_text(str),
+            "choices": [{"x": x, "y": y, "w": w, "h": h, "text": choice_text, "color": color}....]}
     :return:
     """
-    if False:
-        # 能从数据库中找到答案
-        pass
+    global last_status
+    ok, answer = find_answer(information)
+    if ok:
+        # 能从数据库中找到答案,就直接选择答案
+        select_answer(information, answer)
     else:
-        # 不能从数据库中找到答案
+        # 不能从数据库中找到答案,先把题目和选项存入数据库
         cur.execute("INSERT INTO Questions (question) VALUES(?)", (information['result']['question'],))
         question_id = cur.lastrowid
         for choice in information['result']['choices']:
             cur.execute("INSERT INTO QuestionAns(QUESTION_ID, CHOICE) VALUES(?,?)", (question_id, choice))
         con.commit()
 
+        result = generate_answer(information)
+        last_status = {'type': 'first', 'question': information['question'], 'answer': result}
+        select_answer(information, result)
+
+
+def find_answer(question):
+    """
+    从数据库中找到答案,模糊匹配!!!
+    :param question:{"question": question_text(str),
+            "choices": [{"x": x, "y": y, "w": w, "h": h, "text": choice_text, "color": color}....]}
+    :return:True,{"x": x, "y": y, "w": w, "h": h, "text": choice_text, "color": color}; False,None
+    """
+    result = process.extractOne(question['question'], questions_db)
+    if result[1] < 90:
+        return False, None
+    else:
+        db_answers = cur.execute("select choice,ok from QuestionAns join Questions Q "
+                                 "on QuestionAns.question_id = Q.id where Q.question=?", (result[0],)).fetchall()
+        for choices in question['choices']:
+            res = process.extractOne(choices['text'], [item[0] for item in db_answers])
+            if res[1] < 90:
+                return False, None
+            # TODO: 修改!!!!
+            for db_answer in db_answers:
+                if db_answer[0] == res[0]:
+                    if db_answer[1] == 1:
+                        return True, db_answer[0]
+                    else:
+                        break
+
+
+def generate_answer(question):
+    """
+    对于无法找到的问题生成答案,问GPT或者随机
+    :param question:
+    :return:
+    """
+    return "Right Answer"
+
+
+def select_answer(information, answer):
+    """
+    选择答案
+    :param information:{"question": question_text(str),
+            "choices": [{"x": x, "y": y, "w": w, "h": h, "text": choice_text, "color": color}....]}
+    :param answer: str 要与information模糊匹配
+    :return:
+    """
     pass
 
 
